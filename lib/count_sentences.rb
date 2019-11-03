8f2f190a0d4bebb9b29bf64e3afc0af676f40909
Anika Christopher <anika.mchristopher@gmail.com>
2019-11-03 21:56:32 +0000

    Automatically backed up by Learn

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,24 +3,15 @@ require 'pry'
 class String
   
   def sentence?
-    if self.end_with?(".")
-      return true
-    else
-      return false 
+    self.end_with?(".")
   end
 
   def question?
-    if self.end_with?("?")
-      return true
-    else
-      return false
+    self.end_with?("?")
   end
 
   def exclamation?
-    if self.end_with?("!")
-      return true
-    else 
-      return false
+    self.end_with?("!")
   end
 
   def count_sentences
