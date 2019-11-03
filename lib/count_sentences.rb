d038fea7c0a42ff0390b21b6ea27c62543a43b09
Anika Christopher <anika.mchristopher@gmail.com>
2019-11-03 21:55:30 +0000

    Automatically backed up by Learn

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -1,52 +1,28 @@
-# require 'pry'
-
-# class String
-  
-#   def sentence?
-#     if self.end_with?(".")
-#       return true
-#     else
-#       return false 
-#   end
-
-#   def question?
-#     if self.end_with?("?")
-#       return true
-#     else
-#       return false
-#   end
-
-#   def exclamation?
-#     if self.end_with?("!")
-#       return true
-#     else 
-#       return false
-#   end
-
-#   def count_sentences
-#     self.split(/[.!?]/).reject {|x| x.empty?}.size
-#   end
-  
-#   def count_sentences_words
-#     self.split(/[.!?]/).map{|x| !(x.match(/\w+/).nil?)}.reject{|x| x == false}.size 
-#   end
-# end
-
+require 'pry'
 
 class String
-
+  
   def sentence?
-    self.end_with?(".")
+    if self.end_with?(".")
+      return true
+    else
+      return false 
   end
 
   def question?
-    self.end_with?("?")
+    if self.end_with?("?")
+      return true
+    else
+      return false
   end
-  
+
   def exclamation?
-    self.end_with?("!")
+    if self.end_with?("!")
+      return true
+    else 
+      return false
   end
-  
+
   def count_sentences
     self.split(/[.!?]/).reject {|x| x.empty?}.size
   end
@@ -54,5 +30,29 @@ class String
   def count_sentences_words
     self.split(/[.!?]/).map{|x| !(x.match(/\w+/).nil?)}.reject{|x| x == false}.size 
   end
-
 end
+
+
+# class String
+
+#   def sentence?
+#     self.end_with?(".")
+#   end
+
+#   def question?
+#     self.end_with?("?")
+#   end
+  
+#   def exclamation?
+#     self.end_with?("!")
+#   end
+  
+#   def count_sentences
+#     self.split(/[.!?]/).reject {|x| x.empty?}.size
+#   end
+  
+#   def count_sentences_words
+#     self.split(/[.!?]/).map{|x| !(x.match(/\w+/).nil?)}.reject{|x| x == false}.size 
+#   end
+
+# end
