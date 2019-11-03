fdba3c289cad63222198e9a3526f2146e7cda900
Anika Christopher <anika.mchristopher@gmail.com>
2019-11-03 21:53:39 +0000

    Automatically backed up by Learn

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -1,34 +1,33 @@
-require 'pry'
+# require 'pry'
 
-class String
+# class String
   
-  def sentence?
-    if self.end_with?(".")
-      return true
-    else
-      return false 
-  end
+#   def sentence?
+#     if self.end_with?(".")
+#       return true
+#     else
+#       return false 
+#   end
 
-  def question?
-    if self.end_with?("?")
-      return true
-    else
-      return false
-  end
+#   def question?
+#     if self.end_with?("?")
+#       return true
+#     else
+#       return false
+#   end
 
-  def exclamation?
-    if self.end_with?("!")
-      return true
-    else 
-      return false
-  end
+#   def exclamation?
+#     if self.end_with?("!")
+#       return true
+#     else 
+#       return false
+#   end
 
-  def count_sentences
-    self.split(/[.!?]/).reject {|x| x.empty?}.size
-  end
+#   def count_sentences
+#     self.split(/[.!?]/).reject {|x| x.empty?}.size
+#   end
   
-  def count_sentences_words
-    self.split(/[.!?]/).map{|x| !(x.match(/\w+/).nil?)}.reject{|x| x == false}.size 
-
-  end
-end
+#   def count_sentences_words
+#     self.split(/[.!?]/).map{|x| !(x.match(/\w+/).nil?)}.reject{|x| x == false}.size 
+#   end
+# end
