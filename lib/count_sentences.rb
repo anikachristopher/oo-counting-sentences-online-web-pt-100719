603e401aaf33e1ae9ca457f227b9f580034f43a6
Anika Christopher <anika.mchristopher@gmail.com>
2019-11-03 21:56:54 +0000

    Automatically backed up by Learn

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -22,28 +22,3 @@ class String
     self.split(/[.!?]/).map{|x| !(x.match(/\w+/).nil?)}.reject{|x| x == false}.size 
   end
 end
-
-
-# class String
-
-#   def sentence?
-#     self.end_with?(".")
-#   end
-
-#   def question?
-#     self.end_with?("?")
-#   end
-  
-#   def exclamation?
-#     self.end_with?("!")
-#   end
-  
-#   def count_sentences
-#     self.split(/[.!?]/).reject {|x| x.empty?}.size
-#   end
-  
-#   def count_sentences_words
-#     self.split(/[.!?]/).map{|x| !(x.match(/\w+/).nil?)}.reject{|x| x == false}.size 
-#   end
-
-# end
